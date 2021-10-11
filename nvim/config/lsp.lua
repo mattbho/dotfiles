2f4e737588eab6879d029b87c6b307bb364f4515
mattbho <mtthwbh@gmail.com>
2021-10-11 12:35:45 -0700

    remove deprecated code from cmp

--- nvim/config/lsp.lua
+++ nvim/config/lsp.lua
@@ -101,25 +101,19 @@ require("lspconfig")["null-ls"].setup({
 
 local cmp = require 'cmp'
 local lspkind = require 'lspkind'
-local luasnip = require 'luasnip'
 
 cmp.setup {
-  completion = {
-    completeopt='menu,menuone,noinsert,noselect',
-  },
-  formatting = {
-    format = require("lspkind").cmp_format({with_text = true, menu = ({
-      buffer = "[Buffer]",
-      nvim_lsp = "[LSP]",
-      luasnip = "[LuaSnip]",
-      nvim_lua = "[Lua]",
-      latex_symbols = "[Latex]",
-    })}),
-  },
   snippet = {
     expand = function(args)
       require('luasnip').lsp_expand(args.body)
-    end,
+    end
+  },
+  formatting = {
+    format = lspkind.cmp_format({with_text = true, menu = ({
+      buffer = "[Buffer]",
+      nvim_lsp = "[LSP]",
+      luasnip = "[LuaSnip]",
+    })}),
   },
   mapping = {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -128,24 +122,17 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+    ['<CR>'] = cmp.mapping.confirm { select = true },
     ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-      elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      if cmp.visible() then
+        cmp.select_next_item()
       else
         fallback()
       end
     end,
     ['<S-Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-      elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      if cmp.visible() then
+        cmp.select_prev_item()
       else
         fallback()
       end
